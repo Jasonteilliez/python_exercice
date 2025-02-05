764f003dd4b17a9894b40b3ed5a1a241d197192f
Jason Teilliez <jason.teilliez@gmail.com>
2025-02-05 12:55:43 +0100

    create exo 9

---
+++ Exercice/Exercice_9/main.py
@@ -0,0 +1,9 @@
+def first_non_repeating_letter(string):
+    string_lower = string.lower()
+    for i, letter in enumerate(string_lower):
+        if string_lower.count(letter) == 1:
+            return string[i]
+            
+    return ""
+
+print(first_non_repeating_letter('sTreSS'))
